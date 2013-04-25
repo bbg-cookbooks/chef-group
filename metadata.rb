ace4a8bc782d980ea276b82b249c887d762cc8f0
Chris Lundquist <chris.lundquist@bluebox.net>
2013-04-25 13:22:41 -0700

    [test-kitchen] add basic run test

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,5 @@ maintainer_email "support@bluebox.net"
 license          "Apache 2.0"
 description      "Installs/Configures group"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
+name             "group"
